91e9e413bef14988c7b89e10549fbb9985e6ba9e
Baptiste Laborde <balaborde.pro@gmail.com>
2023-09-26 17:01:24 +0200

    feat: added microservice times from the openapi doc

--- showtime/showtime.py
+++ showtime/showtime.py
@@ -8,12 +8,28 @@ PORT = 3202
 HOST = '0.0.0.0'
 
 with open('{}/databases/times.json'.format("."), "r") as jsf:
-   schedule = json.load(jsf)["schedule"]
+    schedule = json.load(jsf)["schedule"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
+
+
+@app.route("/showtimes", methods=['GET'])
+def get_showtimes():
+    res = make_response(jsonify(schedule), 200)
+    return res
+
+@app.route("/showmovies/<date>", methods=['GET'])
+def get_movies_on_date(date: str):
+    for times in schedule:
+        if times["date"] == date:
+            res = make_response(jsonify(times["date"], times["movies"]), 200)
+            return res
+    return make_response(jsonify({"error": "No movie scheduled at this date"}), 400)
+
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
