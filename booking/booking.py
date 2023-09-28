881b779e016856911ef5f478b05d65775ff1cc3a
Frédéric Egenscheviller <53983944+frederic-egenscheviller@users.noreply.github.com>
2023-09-28 16:53:09 +0200

    feat: add booking's routes

--- booking/booking.py
+++ booking/booking.py
@@ -9,13 +9,42 @@ PORT = 3201
 HOST = '0.0.0.0'
 
 with open('{}/databases/bookings.json'.format("."), "r") as jsf:
-   bookings = json.load(jsf)["bookings"]
+    bookings = json.load(jsf)["bookings"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
+
+
+@app.route("/bookings", methods=['GET'])
+def get_json():
+    res = make_response(jsonify(bookings), 200)
+    return res
+
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+    for booking in bookings:
+        if str(booking["userid"]) == str(userid):
+            res = make_response(jsonify(booking), 200)
+            return res
+    return make_response(jsonify({"error": "Booking User ID not found"}), 400)
+
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid):
+    req = request.get_json()
+
+    for booking in bookings:
+        if booking["dates"] == req["dates"]:
+            return make_response(jsonify({"error": "booking already exists for this day"}), 409)
+
+    bookings.append(req)
+    res = make_response(jsonify({"message": "booking added"}), 200)
+    return res
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
