207aeda7417c1d0628e6564f2d446b10fc6d1dea
Baptiste Laborde <balaborde.pro@gmail.com>
2023-10-03 17:46:44 +0200

    feat: added microservice user

--- user/user.py
+++ user/user.py
@@ -8,14 +8,51 @@ app = Flask(__name__)
 PORT = 3203
 HOST = '0.0.0.0'
 
+MOVIE_SERVICE_URL = "http://localhost:3200"
+BOOKING_SERVICE_URL = "http://localhost:3201"
+
 with open('{}/databases/users.json'.format("."), "r") as jsf:
-   users = json.load(jsf)["users"]
+    users = json.load(jsf)["users"]
+
 
 @app.route("/", methods=['GET'])
 def home():
-   return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+    return "<h1 style='color:blue'>Welcome to the User service!</h1>"
+
+
+@app.route("/user-bookings/<userid>", methods=['GET'])
+def get_user_bookings(userid):
+    booking_response = requests.get(f"{BOOKING_SERVICE_URL}/bookings/{userid}")
+
+    if booking_response.status_code == 200:
+        user_bookings = booking_response.json()
+        return make_response(jsonify(user_bookings), 200)
+    else:
+        return make_response(jsonify({"error": "User ID not found in Booking service"}), 400)
+
+
+@app.route("/user-bookings/<userid>/detailed", methods=['GET'])
+def get_detailed_userbookings(userid):
+    booking_response = requests.get(f"{BOOKING_SERVICE_URL}/bookings/{userid}")
+
+    if booking_response.status_code == 200:
+        user_bookings = booking_response.json()
+        movie_infos = []
+        for booking in user_bookings["dates"]:
+            for movie in booking["movies"]:
+                movie_response = requests.get(f"{MOVIE_SERVICE_URL}/movies/{movie}")
+                movie_infos.append(movie_response.json())
+
+        movie_info_map = {movie['id']: movie for movie in movie_infos}
+        for date_info in user_bookings['dates']:
+            for i, movie_id in enumerate(date_info['movies']):
+                date_info['movies'][i] = movie_info_map.get(movie_id, {})
+
+        return make_response(jsonify(user_bookings), 200)
+    else:
+        return make_response(jsonify({"error": "User ID not found in Booking service"}), 400)
 
 
 if __name__ == "__main__":
-   print("Server running in port %s"%(PORT))
-   app.run(host=HOST, port=PORT)
+    print("Server running in port %s" % (PORT))
+    app.run(host=HOST, port=PORT)
